c9b1192741c627e0e9c1b868b1508b40c7f24820
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-05-15 17:24:29 -0300

    Adding new rooms completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
   before_action :authenticate_user!
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,33 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+    authorize @room
+  end
+
+  def create
+    @room = Room.new(room_params)
+    authorize @room
+    if @room.save
+      redirect_to dashboard_rooms_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(
+      :name,
+      :quantity,
+      :bed,
+      :bathroom,
+      :price,
+      :description
+    )
+  end
+
 end
