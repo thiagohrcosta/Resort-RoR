70908794452f817886e4703b2663fcdc7cc9a168
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-05-15 15:30:58 -0300

    Adding rooms controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
+require 'uri'
+
 User.destroy_all
+Room.destroy_all
 
 user_1 = User.create(
   email: 'admin@admin.com',
   password: '123456',
   password_confirmation: '123456',
   profile: 0
+)
+
+room_1 = Room.create(
+  quantity: 8,
+  bed: 1,
+  bathroom: 1,
+  price: 499,
 )
